b70d2ed88d5bc699f7b71e3cc589b18d943710a6
yuanjingya <yuanjingya@czb365.com>
2020-10-13 09:38:43 +0800

    feat: test

---
+++ testModel.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="hello">
+<input type="text" v-myModel="num"> <button @click="num++">click</button> <p>{{num}}</p>
+</div>
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      num: 0
+    }
+  },
+  directives: {
+    myModel: {
+      bind: (el, obj, Vnode) => {
+        el.value = obj.value
+        el.addEventListener('input', function () {
+          Vnode.context[obj.expression] = this.value
+        })
+      },
+      update: (el, obj) => {
+        el.value = obj.value
+      }
+    }
+  }
+}
+</script>
